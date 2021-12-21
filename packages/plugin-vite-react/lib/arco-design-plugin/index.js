070996bdab5b61c1744ffa201c6b2a5e027fe91b
jinghongming <jinghongming@bytedance.com>
2021-12-21 15:39:35 +0800

    chore:remove useless code

--- packages/plugin-vite-react/lib/arco-design-plugin/index.js
+++ packages/plugin-vite-react/lib/arco-design-plugin/index.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const less_1 = require("./less");
 const icon_1 = require("./icon");
 const transform_1 = require("./transform");
-const config_1 = require("./config");
 const pkg = require('../../package.json');
 function vitePluginArcoImport(options = {}) {
     const { theme = '', iconBox = '', modifyVars = {}, style = true } = options;
@@ -29,29 +28,6 @@ function vitePluginArcoImport(options = {}) {
             (0, less_1.modifyCssConfig)(config, theme, modifyVars);
             // iconbox
             (0, icon_1.modifyIconConfig)(config, iconBoxLib);
-            const plugin = {
-                name: 'test',
-                setup(build) {
-                    console.log('originjs:commonjs');
-                    build.onLoad({
-                        filter: new RegExp(`${config_1.libraryName}/lib/index.js`),
-                        namespace: 'file',
-                    }, async ({ path }) => {
-                        console.log('path', path);
-                        // const code = fs.readFileSync(id).toString();
-                        // let result = lib_1.transformRequire(code, id);
-                        // if (result.replaced) {
-                        //   console.log('id', id);
-                        //   return {
-                        //     contents: result.code,
-                        //     loader: 'js',
-                        //   };
-                        // }
-                        return null;
-                    });
-                },
-            };
-            config.optimizeDeps.esbuildOptions.plugins.push(plugin);
         },
         async load(id) {
             const res = (0, icon_1.loadIcon)(id, iconBox, iconBoxLib);
