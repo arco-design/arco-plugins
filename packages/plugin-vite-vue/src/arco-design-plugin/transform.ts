9c3093f261ae6362eaf4afdf64e812dcbcc91a49
番薯有才 <tazytan@qq.com>
2022-10-11 18:57:11 +0800

    feat(plugin-vite-vue): support auto import in template (#34)

--- packages/plugin-vite-vue/src/arco-design-plugin/transform.ts
+++ packages/plugin-vite-vue/src/arco-design-plugin/transform.ts
@@ -4,7 +4,13 @@ import traverse, { NodePath } from '@babel/traverse';
 import generate from '@babel/generator';
 
 import { libraryName, fullCssMatchers } from './config';
-import { getVueComponentDir, importStyle, pathMatch, readFileStrSync } from './utils';
+import {
+  getComponentConfig,
+  importComponent,
+  importStyle,
+  pathMatch,
+  readFileStrSync,
+} from './utils';
 
 interface Specifier {
   imported: {
@@ -72,22 +78,26 @@ export function transformJsFiles({
         const { callee, arguments: args } = node as any;
         const funcName = callee.name;
         const importedName = args?.[0]?.value;
+        // a-input-number => InputNumber
+        const componentName =
+          typeof importedName === 'string'
+            ? importedName
+                .replace(new RegExp(`^${componentPrefix}-`), '')
+                .replace(/(^|-)[a-z]/g, (w) => w.replace('-', '').toUpperCase())
+            : undefined;
         if (
-          !importedName ||
+          !componentName ||
           !['_resolveComponent', '_resolveDynamicComponent'].includes(funcName)
         ) {
           return;
         }
-        const dir = getVueComponentDir(libraryName, importedName, componentPrefix);
-
-        if (dir) {
-          importStyle({
-            componentDirs: [dir],
-            styleOptimization,
+        const componentConfig = getComponentConfig(libraryName, componentName);
+        if (componentConfig) {
+          // 导入组件，此处会触发下面 import 方式的逻辑，所以不需要再导入样式
+          importComponent({
             path,
-            style,
-            theme,
-            libraryName,
+            componentDir: componentConfig.dir,
+            componentName,
           });
         }
         return;
@@ -101,12 +111,10 @@ export function transformJsFiles({
           node.specifiers.forEach((spec) => {
             if (types.isImportSpecifier(spec)) {
               const importedName = (spec as Specifier).imported.name;
-              // InputNumber => input-number
-              const dir = importedName.replace(
-                /([A-Z])/g,
-                (_match, p1, index) => `${index > 0 ? '-' : ''}${p1.toLowerCase()}`
-              );
-              dirs.push(dir);
+              const componentConfig = getComponentConfig(libraryName, importedName);
+              if (componentConfig?.styleDir) {
+                dirs.push(componentConfig.styleDir);
+              }
             }
           });
           importStyle({
