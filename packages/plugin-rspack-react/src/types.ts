8ef944b6d0d02e4e6efd0c29d1df86dbcbe45680
Asuka109 <asuka109@foxmail.com>
2023-04-24 19:25:48 +0800

    feat: remove useless typings

--- packages/plugin-rspack-react/src/types.ts
+++ packages/plugin-rspack-react/src/types.ts
@@ -1,15 +1,15 @@
 export interface ArcoDesignPluginOptions {
-  /** @deprecated The pluginImport feature of Rspack will affect all files. */
-  include?: (string | RegExp)[];
-  extensions?: string[];
+  // /** @deprecated The pluginImport feature of Rspack will affect all files. */
+  // include?: (string | RegExp)[];
+  // extensions?: string[];
   style?: string | boolean;
   libraryDirectory?: string;
   iconBox?: string;
   removeFontFace?: boolean;
   defaultLanguage?: string;
   theme?: string;
-  /** @deprecated `modifyVars` should be config in options of less-loader manually. */
-  modifyVars?: Record<string, string>;
-  /** @deprecated `varsInjectScope` should be config in options of less-loader manually. */
-  varsInjectScope?: (string | RegExp)[];
+  // /** @deprecated `modifyVars` should be config in options of less-loader manually. */
+  // modifyVars?: Record<string, string>;
+  // /** @deprecated `varsInjectScope` should be config in options of less-loader manually. */
+  // varsInjectScope?: (string | RegExp)[];
 }
