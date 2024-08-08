880da2a8cd811f5803a306ca791cb8cdf63771ed
idler <zqc.sunny@gmail.com>
2024-08-08 14:36:25 +0800

    fix(unplugin): correct unplugin-react plugin due to rspack deprecated buildin option
    * fix(unplugin): correct unplugin-react plugin due to rspack deprecated buildin option
    
    * fix(unplugin): Add compatible code
    
    * fix(unplugin): remove useless code
    
    * fix(unplugin): correct rules for import
    
    * fix(unplugin): correct rules for replace import
    
    ---------
    
    Co-authored-by: Justinidlerz <zqc.suny@gmail.com>

--- packages/unplugin-react/src/plugins/import.ts
+++ packages/unplugin-react/src/plugins/import.ts
@@ -10,27 +10,64 @@ export class ImportPlugin {
   }
 
   apply(compiler: Compiler) {
-    compiler.options.builtins.pluginImport ||= [];
+    /**
+     * Compatible with the new rspack version
+     * due to since 0.63 removed options.builtins of compiler
+     * https://github.com/web-infra-dev/rspack/releases/tag/v0.6.3
+     */
+    if (compiler.options.builtins) {
+      compiler.options.builtins.pluginImport ||= [];
 
-    compiler.options.builtins.pluginImport.push({
-      libraryDirectory: this.options.libraryDirectory || 'es',
-      style: this.options.style ?? true,
-      libraryName: ARCO_DESIGN_COMPONENT_NAME,
-      camelToDashComponentName: false,
-    });
-
-    compiler.options.builtins.pluginImport.push({
-      libraryName: ARCO_DESIGN_ICON_NAME,
-      libraryDirectory: 'react-icon',
-      camelToDashComponentName: false,
-    });
-
-    if (this.options.iconBox) {
       compiler.options.builtins.pluginImport.push({
-        libraryName: this.options.iconBox,
-        libraryDirectory: 'esm',
+        libraryDirectory: this.options.libraryDirectory || 'es',
+        style: this.options.style ?? true,
+        libraryName: ARCO_DESIGN_COMPONENT_NAME,
         camelToDashComponentName: false,
       });
+
+      compiler.options.builtins.pluginImport.push({
+        libraryName: ARCO_DESIGN_ICON_NAME,
+        libraryDirectory: 'react-icon',
+        camelToDashComponentName: false,
+      });
+
+      if (this.options.iconBox) {
+        compiler.options.builtins.pluginImport.push({
+          libraryName: this.options.iconBox,
+          libraryDirectory: 'esm',
+          camelToDashComponentName: false,
+        });
+      }
+    } else {
+      compiler.options.module.rules ||= [];
+
+      const rule = {
+        test: /(jsx?|tsx?)$/,
+        loader: 'builtin:swc-loader',
+        options: {
+          rspackExperiments: {
+            import: [
+              {
+                customName: `${ARCO_DESIGN_COMPONENT_NAME}/${
+                  this.options.libraryDirectory || 'es'
+                }/{{member}}`,
+                style: this.options.style ?? true,
+                libraryName: ARCO_DESIGN_COMPONENT_NAME,
+              },
+              {
+                libraryName: ARCO_DESIGN_ICON_NAME,
+                customName: `${ARCO_DESIGN_ICON_NAME}/react-icon/{{member}}`,
+              },
+              {
+                libraryName: this.options.iconBox,
+                customName: `${this.options.iconBox}/esm/{{member}}`,
+              },
+            ],
+          },
+        },
+      };
+
+      compiler.options.module.rules.push(rule);
     }
   }
 }
