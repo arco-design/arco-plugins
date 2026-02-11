ddc8a434eb0deeff791ff9661f710689b30bcb28
Tmk <i@tmk.im>
2026-02-12 00:36:08 +0800

    refactor: replace loader-based icon replacement with direct resource path modification (#73)

--- packages/unplugin-react/src/utils/index.ts
+++ packages/unplugin-react/src/utils/index.ts
@@ -1,14 +1,8 @@
 import assert from 'assert';
-import type { RuleSetLogicalConditions } from '@rspack/core';
 import { makeRe } from 'minimatch';
 
-export const compileGlob = (expr: string): RuleSetLogicalConditions => {
+export const compileGlob = (expr: string): RegExp => {
   const regex = makeRe(expr, { dot: true });
   assert(regex);
-  return {
-    and: [(resource: string) => regex.test(resource)],
-    // toString() {
-    //   return `glob(${expr})`;
-    // },
-  };
+  return regex;
 };
